aebebaf5ba1f205315868c1b6129f5b96392816a
ruiqurm <ruiqurm@gmail.com>
2021-08-21 21:15:10 +0800

    debug vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -294,6 +294,7 @@ freewalk(pagetable_t pagetable)
 void
 uvmfree(pagetable_t pagetable, uint64 sz)
 {
+  // vmprint(pagetable,0);
   if(sz > 0)
     uvmunmap(pagetable, 0, PGROUNDUP(sz)/PGSIZE, 1);
   freewalk(pagetable);
@@ -438,5 +439,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return 0;
   } else {
     return -1;
+  }
+}
+
+
+void
+vmprint(pagetable_t pagetable,int level){
+  if(level==0){
+    printf("page table %p\n",pagetable);
+  }
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    uint64 child = PTE2PA(pte);
+    if(pte & PTE_V){
+      for(int j =0;j<=level;j++){
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n",i,pte,child);
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        // 中间PTE
+        vmprint((pagetable_t)child,level+1);
+      }
+    }
   }
 }
