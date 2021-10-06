1e06eb6788bbf82b2d06f81d666cec18a0ebee84
zertow <1181527496@qq.com>
2021-10-06 19:31:38 +0800

    end

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,15 +6,15 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
-
+#include "sysinfo.h"
 uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +44,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,12 +58,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +80,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +96,46 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int n;
+  if (argint(0, &n) < 0)
+  {
+    return -1;
+  }
+  struct proc *p = myproc();
+  int i = 0;
+  while (i < 23 && n > 0)
+  {
+    if (n % 2)
+    {
+      p->mask[i++] = '1';
+    }
+    else
+    {
+      p->mask[i++] = '0';
+    }
+    n >>= 1;
+  }
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 addr;
+  struct sysinfo tem;
+  struct proc *p = myproc();
+  tem.freemem = get_free_size();
+  tem.nproc = get_free_proc_number();
+  if (argaddr(0, &addr) < 0)
+  {
+    return -1;
+  }
+  if (copyout(p->pagetable, addr, (char *)&tem, sizeof(tem)) < 0)
+    return -1;
+  return 0;
 }
